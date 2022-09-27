04878b9040d4854cc3521902dfbefc9a636dcad3
ReadinGravity <113469951+ReadinGravity@users.noreply.github.com>
2022-09-27 22:35:09 +0200

    while2

--- while2.py
+++ while2.py
@@ -3,19 +3,9 @@
 
 cislo=int(input("zadaj mi ciselko: "))
 pocet=0
-x=cislo
 while cislo<=100:
-      cislo=int(input("zadaj cislo: "))
-      x+=cislo
-      pocet+=1
+    cislo+=cislo+1
+    pocet+=1
 print(cislo)
 print(pocet)
 
-cislo=int(input("zadaj cislo: "))
-pocet=0
-x=cislo
-while cislo!=0:
-    cislo=int(input("zadaj cislo: "))
-    x+=cislo
-    pocet+=1
-print(x/pocet)
