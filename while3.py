bae863b11c926f3959b60a36e57cded6535b21b6
ReadinGravity <113469951+ReadinGravity@users.noreply.github.com>
2022-09-27 22:33:15 +0200

    Delete while3.py

--- while3.py
+++
@@ -1,7 +0,0 @@
-# napíš program, ktorý bude V CYKLE načítavať mená a vypisvať ich dĺžku dovtedy,
-# poikiaľ ako meno nezdáte reťazec "koniec"
-
-meno=input("napis mi meno: ")
-while meno!="koniec":
-    print(len(meno))
-    meno=input("napis mi meno: ")
